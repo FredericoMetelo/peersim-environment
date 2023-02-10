f16c0c71247be1b0342d4d62aa9ee183b28a918a
FredericoMetelo <fc.metelo@campus.fct.unl.pt>
2023-02-10 16:22:21 +0000

    Adding files that where blocked in .gitignore but are important.W

---
+++ install-mvn-external-dependencies.ps1
@@ -0,0 +1,3 @@
+mvn install:install-file -Dfile=Setup\external\peersim-repo\djep\1.0.0\djep-1.0.0.jar -DgroupId=peersim-repo -DartifactId=djep -Dversion=1.0.0 -Dpackaging=jar -DcreateChecksum=true
+mvn install:install-file -Dfile=Setup\external\peersim-repo\jep\2.3.0\jep-2.3.0.jar -DgroupId=peersim-repo -DartifactId=jep -Dversion=2.3.0 -Dpackaging=jar -DcreateChecksum=true
+mvn install:install-file -Dfile=Setup\external\peersim-repo\peersim-doclet\1.0.0\peersim-doclet.jar -DgroupId=peersim-repo -DartifactId=peersim-doclet -Dversion=1.0.0 -Dpackaging=jar -DcreateChecksum=true
