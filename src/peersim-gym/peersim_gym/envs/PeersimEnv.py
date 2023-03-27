7093dc28a9ab85d7b42299970fe42a9995e8c44a
FredericoMetelo <fc.metelo@campus.fct.unl.pt>
2023-03-27 15:18:41 +0100

    Fixed bugs, passed the configuration of the environement in its totallyty to the dictionary. No mopre parameter in the constructor. Default values where kept the same.

--- src/peersim-gym/peersim_gym/envs/PeersimEnv.py
+++ src/peersim-gym/peersim_gym/envs/PeersimEnv.py
@@ -15,35 +15,63 @@ from peersim_gym.envs.PeersimThread import PeersimThread
 class PeersimEnv(gym.Env):
     metadata = {"render_modes": ["ansi"], "render_fps": 4}
 
-    def __init__(self, render_mode=None, number_nodes=10, max_Q_size=10, max_w=1, configs=None, log_dir=None):
+    def __init__(self, render_mode=None, configs=None, log_dir=None):
         # ==== Variables to configure the PeerSim
-        self.number_nodes = number_nodes
-        self.max_Q_size = max_Q_size
-        self.max_w = max_w
+        self.number_nodes = 10
+        self.max_Q_size = 10
+        self.max_w = 1
 
         self.url_api = "http://localhost:8080"
         self.url_action_path = "/action"
         self.url_state_path = "/state"
         self.url_isUp = "/up"
 
+        # Checking the configurations
+        if configs is None:
+            configs = {"protocol.wrk.Q_MAX": str(self.max_Q_size), "SIZE": str(self.number_nodes)}
+
+            cg.generate_config_file(configs)
+            self.config_path = cg.TARGET_FILE_PATH
+
+        elif type(configs) is dict:
+            # Consistency of QMax in configs and the arguments.
+            if "protocol.wrk.Q_MAX" in configs:
+                self.max_Q_size = int(configs["protocol.wrk.Q_MAX"])
+
+            if "SIZE" in configs:
+                self.number_nodes = int(configs["SIZE"])
+
+            cg.generate_config_file(configs)
+            self.config_path = cg.TARGET_FILE_PATH
+
+        elif type(configs) is str:
+            self.config_path = configs
+
+        else:
+            raise Exception(
+                "Invalid Type for the configs parameter. Needs to be None, a Dictionary or a String. Please see the project README.md")
+
         # ==== Environment Definition
         # ---- State Space
         self.__log_dir = log_dir
         self.__run_counter = 0
 
-        if isinstance(max_Q_size, list):
-            if len(max_Q_size) != number_nodes:
+
+        if isinstance(self.max_Q_size, list):
+
+            if len(self.max_Q_size) != self.number_nodes:
                 print("The number of entries in max_Q_size needs to be equal to"
                       " the number of nodes. Or then max_Q_size needs to be just a number")
                 return
-            q_list = max_Q_size
+            q_list = self.max_Q_size
+            raise Exception("Nodes with diferent max lenghts is not supported yet!")  # TODO rectify this!!
         else:
-            q_list = [max_Q_size for _ in range(number_nodes)]
+            q_list = [self.max_Q_size for _ in range(self.number_nodes)]
         self.observation_space = Dict(
             {
-                "n_i": Discrete(number_nodes, start=1),
+                "n_i": Discrete(self.number_nodes, start=1),
                 "Q": MultiDiscrete(q_list),
-                "w": Box(high=max_w, low=0, dtype=np.float)
+                "w": Box(high=self.max_w, low=0, dtype=np.float)
                 # The authors use a Natural number to represent this, value. I use a continuous value, because the way
                 # I interpreted the W is how many tasks are bing processed by the node in one unit of time.
                 # I assume that 100 ticks makes one unit of time (100 ticks is the time it takes for all nodes to take
@@ -55,8 +83,8 @@ class PeersimEnv(gym.Env):
         # ---- Action Space
         self.action_space = Dict(
             {
-                "target_node": Discrete(number_nodes-1, start=1),
-                "offload_amount": Discrete(max_Q_size, start=1)
+                "target_node": Discrete(self.number_nodes - 1, start=1),
+                "offload_amount": Discrete(self.max_Q_size, start=1)
             }
         )
 
@@ -64,33 +92,13 @@ class PeersimEnv(gym.Env):
         # Run the actual environment.
         # Prepare the Configuration file
 
-        if configs is None or type(configs) is dict:
-            # Consistency of QMax in configs and the arguments.
-            if configs is not None and "protocol.wrk.Q_MAX" in configs:
-                if configs["protocol.wrk.Q_MAX"] != str(self.max_Q_size):
-                    raise Exception("Argument QMAX and the configuration protocol.wrk.Q_MAX in the configuration "
-                                    "dictionary must be equal. Please specify the QMAX parameter.")
-            elif configs is not None:
-                configs["protocol.wrk.Q_MAX"] = str(self.max_Q_size)
-            else:
-                # configs is None
-                configs = {"protocol.wrk.Q_MAX": str(self.max_Q_size)}
-
-            cg.generate_config_file(configs)
-            self.config_path = cg.TARGET_FILE_PATH
-
-        elif type(configs) is str:
-            self.config_path = configs
-        else:
-            raise Exception(
-                "Invalid Type for the configs parameter. Needs to be None, a Dictionary or a String. Please see the project README.md")
         # with subprocess as s:
         self.simulator = None
         # self.simulator = PeersimThread(name='Run0', configs=self.config_path)
         # self.simulator.run()
 
-    def init(self, render_mode=None, number_nodes=10, max_Q_size=10, max_w=1, configs=None, log_dir=None):
-        self.__init__(render_mode, number_nodes, max_Q_size, max_w, configs, log_dir=log_dir)
+    def init(self, render_mode=None, configs=None, log_dir=None):
+        self.__init__(render_mode, configs, log_dir=log_dir)
 
     def reset(self, **kwargs):
         if self.simulator != None:
@@ -121,8 +129,10 @@ class PeersimEnv(gym.Env):
 
     def observation_space_shape(self):
         return self.observation_space.sample().shape()
+
     def action_space_shape(self):
         return self.action_space.sample().shape()
+
     def __see_types(self):
         print("Example of action.")
         print(self.action_space.sample())
@@ -132,7 +142,7 @@ class PeersimEnv(gym.Env):
     def __run_peersim(self):
         self.__run_counter += 1
         log_file = None
-        if not (self.__log_dir  == None):
+        if not (self.__log_dir == None):
             log_file = self.__log_dir + f'log_run_{self.__run_counter}.txt'
             if os.path.exists(log_file):
                 os.remove(log_file)
