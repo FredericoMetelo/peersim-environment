265dd3c81209f3dc1916b35f3b33c5f16615d678
FredericoMetelo <fc.metelo@campus.fct.unl.pt>
2023-07-04 22:22:57 +0100

    I touched on a completely unrelated part of the code... But ended up breaking something in the simulation... Im just commiting to check the diffs.

--- target/classes/README.md
+++
@@ -1,9 +0,0 @@
-# Gym Peersim
-
-### Environments
-This repository hosts the examples that are shown [on the environment creation documentation](https://gymnasium.farama.org/tutorials/environment_creation/).
-- `PeersimEnv`: Environment that allows interaction with the peersim simulation tool.
-
-### Wrappers
-None
-
--- target/classes/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="peersim_gym",
-    version="0.0.1",
-    install_requires=["gymnasium==0.26.3", "requests==2.28.1"],
-    package_dir={'peersim-gym': 'src/peersim-gym'},
-    package_data={'peersim-gym': ['peersim_gym/envs/configs/*.txt', 'peersim_gym/envs/Environment/*']}
-)
